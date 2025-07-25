22fab682482f2232ef73e9543e8c4dad6f01220a
Yogeshwar Saini <144199808+yogeshwarsaini@users.noreply.github.com>
2025-07-26 01:50:19 +0530

    Update main.tf

--- terraform-strapi/main.tf
+++ terraform-strapi/main.tf
@@ -80,7 +80,7 @@ resource "aws_instance" "strapi_ec2" {
 
 # 🔹 Application Load Balancer
 resource "aws_lb" "strapi_alb" {
-  name               = "strapi-alb"
+  name               = "strapi-alb-v2"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
